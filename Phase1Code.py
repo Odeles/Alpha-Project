2559929be5a8f2820fe31ca24fe2021741742270
Ammiad Odels <amiad.odles@gmail.com>
2026-06-10 20:04:40 +0300

    Add Phase 1 code and results screenshot

--- Phase1Code.py
+++ Phase1Code.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 
 dataset = BNCI2014_001()
 paradigm = MotorImagery(
+    events=["left_hand","right_hand"],
     n_classes=2,           # Binary simplification (Left vs. Right)
     fmin=8.0, fmax=32.0,   # Bandpass filter (Alpha/Beta bands)
     tmin=0.5, tmax=2.5     # Epoching (0.5s to 2.5s post-cue)
@@ -19,10 +20,15 @@ dataset.subject_list = [1]
 # y = The labels ('left_hand' or 'right_hand')
 X, y, metadata = paradigm.get_data(dataset=dataset, subjects=[1])
 
-#Step 1: define the increments
-N = list(range(50, X.shape[0] - 30, 10)) # create a list with every multiple of ten from 50 to the maximum. I know the instructions said from 10, but there are 22 channels,
-# which Claude AI said would give garbage results or break completely for the first few iterations
-# and we cut it off 3 intervals sooner, because otherwise there arent enough tests remaining for the final few iterations, leading to an artificial drop in accuracy
+# Step 0.1: Separate training and testing data
+train_mask = metadata['session'] == '0train'
+test_mask  = metadata['session'] == '1test'
+
+X_train_all = X[train_mask]
+y_train_all = y[train_mask]
+
+# Step 1: define the increments
+N = list(range(50, X_train_all.shape[0] - 30, 10))
 
 # Step 2: Prepare storage
 lda_accuracies = []
@@ -30,11 +36,11 @@ qda_accuracies = []
 
 #Step 3: Start a loop
 for i in N:
-    #Step 4: Slice data
-    X_train = X[:i]
-    y_train = y[:i]
-    X_test = X[i:]
-    y_test = y[i:]
+    # Step 4: Slice the data
+    X_train = X_train_all[:i]
+    y_train = y_train_all[:i]
+    X_test = X[test_mask]
+    y_test = y[test_mask]
 
     # Step 5: Constructing the LDA pipeline
     lda_pipeline = Pipeline([
@@ -45,7 +51,7 @@ for i in N:
     # Step 6: Constructing the QDA pipeline
     qda_pipeline = Pipeline([
         ('csp', CSP(n_components=4, reg=None, log=True)),
-        ('qda', QDA())
+        ('qda', QDA(reg_param=0.1))
     ])
 
     lda_pipeline.fit(X_train, y_train)   # CSP learns filters, LDA learns boundaries
@@ -57,7 +63,7 @@ for i in N:
     qda_accuracies.append(qda_acc)
 
 print(f"Data shape: {X.shape}") # Expected: (~144 trials, 22 channels, 500 timepoints)
-# Got: (576, 22, 501)
+# Got: (288, 22, 501)
 # Return later to double-check trial count
 
 plt.figure(figsize=(10, 6))
